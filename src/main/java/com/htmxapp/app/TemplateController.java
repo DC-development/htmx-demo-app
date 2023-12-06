ad1186dd48e99a4e45de01589dfaf0f36b375b14
psengelmann <philipp.sengelmann@akquinet.de>
2023-12-06 21:15:00 +0100

    some styling and scss

--- src/main/java/com/htmxapp/app/TemplateController.java
+++ src/main/java/com/htmxapp/app/TemplateController.java
@@ -9,23 +9,23 @@ import java.util.TimerTask;
 @Controller
 public class TemplateController {
 
-    @GetMapping("/")
-    private String getTemplate(Model model) {
-        String[] factorsOf24 = { "1", "2", "3", "4", "6", "12", "24", "25" };
-        model.addAttribute("items", factorsOf24);
-        model.addAttribute("title", "Checking out layouts");
-        model.addAttribute("template", "/components/test/test");
-        return "master";
+  @GetMapping("/")
+  private String getTemplate(Model model) {
+    String[] factorsOf24 = {"1", "2", "3", "4", "6", "12", "24", "25"};
+    model.addAttribute("items", factorsOf24);
+    model.addAttribute("title", "Checking out HTMX");
+    model.addAttribute("template", "/components/list/list");
+    return "master";
+  }
+
+  @GetMapping("/list")
+  private String getList(Model model) {
+    String[] factorsOf24 = {"Lisa", "Bine", "Dummy", "Puma", "Pumukkel", "Dada", "Gugu"};
+    model.addAttribute("items", factorsOf24);
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
     }
-    @GetMapping("/list")
-    private String getList(Model model) {
-        String[] factorsOf24 = { "Lisa", "Bine", "Dummy", "Puma", "Pumukkel", "Dada", "Gugu" };
-        model.addAttribute("items", factorsOf24);
-        try {
-            Thread.sleep(1000);
-        }
-        catch (InterruptedException e) {
-        }
-        return "/components/test/test";
-    }
+    return "/components/list/list";
+  }
 }
