38703fe3f50eb30e4046156d5cc89ce924eecad2
psengelmann <philipp.sengelmann@akquinet.de>
2023-12-14 17:40:54 +0100

    added sortable example and object for list iteration example

---
+++ src/main/java/com/htmxapp/app/ListItem.java
@@ -0,0 +1,12 @@
+package com.htmxapp.app;
+
+public class ListItem {
+  public String title;
+  public Integer id;
+
+  public ListItem(String title, Integer id) {
+    this.title = title;
+    this.id = id;
+  }
+}
+
--- src/main/java/com/htmxapp/app/TemplateController.java
+++ src/main/java/com/htmxapp/app/TemplateController.java
@@ -3,8 +3,12 @@ package com.htmxapp.app;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.TimerTask;
+import javax.lang.model.element.ModuleElement;
+
+import static org.thymeleaf.util.StringUtils.split;
 
 @Controller
 public class TemplateController {
@@ -21,11 +25,28 @@ public class TemplateController {
   @GetMapping("/list")
   private String getList(Model model) {
     String[] factorsOf24 = {"Lisa", "Bine", "Dummy", "Puma", "Pumukkel", "Dada", "Gugu"};
+
+    ListItem myitem1 = new ListItem("my title", 1);
+    ListItem myitem2 = new ListItem("my title", 2);
+    ListItem myitem3 = new ListItem("my title", 3);
+    ListItem myitem4 = new ListItem("my title", 4);
+
+    ListItem[] arrayOfItems = {myitem1, myitem2, myitem3, myitem4};
+
     model.addAttribute("items", factorsOf24);
+    model.addAttribute("arrayOfItems", arrayOfItems);
     try {
       Thread.sleep(1000);
     } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+    return "/components/list/list";
+  }
+
+  @PostMapping("/items")
+  private String postItems(Model model, @RequestBody String list) {
+    String[] itemStrings = split(list, "&");
+    model.addAttribute("items", itemStrings);
     return "/components/list/list";
   }
 }
