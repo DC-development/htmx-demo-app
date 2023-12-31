74b55f048928379f2b7cb8370e8d5543530ff24a
psengelmann <philipp.sengelmann@akquinet.de>
2023-12-31 12:54:08 +0100

    clean up and typo

--- src/main/java/com/htmxapp/app/TemplateController.java
+++ src/main/java/com/htmxapp/app/TemplateController.java
@@ -1,17 +1,11 @@
 package com.htmxapp.app;
 
-import jakarta.servlet.http.HttpServletResponse;
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
-import net.minidev.json.JSONValue;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class TemplateController {
